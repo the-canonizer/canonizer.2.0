18463b4185160fb637ef4347f491b685549a1f90
Karun Gautam <gautamv16@gmail.com>
2018-01-17 18:23:05 +0530

    changes

--- resources/views/topics/statementhistory.blade.php
+++ resources/views/topics/statementhistory.blade.php
@@ -26,22 +26,22 @@
 
 <div class="right-whitePnl">
 <div class="row">
-<div class="col-sm-12">
-	<div class="notifySignPNL">
-    	<div class="col-sm-2">
-        	<div class="red-circle"></div>
-            <div class="circle-txt">Objected</div>
-        </div>
-        <div class="col-sm-2">
-        	<div class="green-circle"></div>
-            <div class="circle-txt">Live</div>
-        </div>
-        <div class="col-sm-2">
-        	<div class="yellow-circle"></div>
-            <div class="circle-txt">Not Live</div>
+    <div class="col-sm-12">
+        <div class="notifySignPNL">
+            <div class="col-sm-2">
+                <div class="red-circle"></div>
+                <div class="circle-txt">Objected</div>
+            </div>
+            <div class="col-sm-2">
+                <div class="green-circle"></div>
+                <div class="circle-txt">Live</div>
+            </div>
+            <div class="col-sm-2">
+                <div class="yellow-circle"></div>
+                <div class="circle-txt">Not Live</div>
+            </div>
         </div>
     </div>
-</div>
 </div>
 <div>
     <div class="col-sm-12 margin-btm-2">
