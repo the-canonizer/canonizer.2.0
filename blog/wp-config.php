1ce8c29e92d9973e8fbb53087874dcc3fc035314
K Gautam <gautamv16@gmail.com>
2018-02-27 16:15:57 +0530

    blog database setup

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'canonizer');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cAn_!0n^iz3r');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
