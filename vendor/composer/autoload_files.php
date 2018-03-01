a2cffec97e8921518da92d06510ed1b776ec4fdc
K Gautam <gautamv16@gmail.com>
2018-03-01 18:23:08 +0530

    namespaces

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -16,4 +16,5 @@ return array(
     'e7223560d890eab89cda23685e711e2c' => $vendorDir . '/psy/psysh/src/Psy/functions.php',
     'f0906e6318348a765ffb6eb24e0d0938' => $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
     '58571171fd5812e6e447dce228f52f4d' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
+    '4a1f389d6ce373bda9e57857d3b61c84' => $vendorDir . '/barryvdh/laravel-debugbar/src/helpers.php',
 );
