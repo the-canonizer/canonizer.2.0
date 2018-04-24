5ed354f53ed5332055cbc56063864889b1a5592e
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2018-04-24 15:08:54 +0530

    Python Selenium Testing Framework For Canonizer

--- Selenium/Config.py
+++ Selenium/Config.py
@@ -1,1 +1,18 @@
+"""
+Set All Basic Configuration required for testing Framework
+"""
+
+DEFAULT_BASE_URL = "https://canonizer.com/canonizer2.0/public/"
+
+DEFAULT_CHROME_DRIVER_LOCATION = "/Users/ashutosh/Projects/Upworks/chromedriver_64/chromedriver_mac"
+
+DEFAULT_USER = "kukreti.ashutosh@gmail.com"
+DEFAULT_PASS = "123456"
+DEFAULT_INVALID_USER = "invaliduser@gmail.com"
+DEFAULT_INVALID_PASSWORD = "invalid_password"
+
+# Registration Page Configuration Paramters
+DEFAULT_FIRST_NAME = "Ashutosh"
+DEFAULT_MIDDLE_NAME = ""
+DEFAULT_LAST_NAME = "Kukreti"
 
