d1a1be4a06e164e738df3af72f37870a54f5fc97
Ashutosh Kukreti <ashutosh@Ashutoshs-MacBook-Pro.local>
2018-07-16 23:20:18 +0530

    Update the files

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -25,7 +25,7 @@
 
                             <div class="form-group">
 
-                                <label for="title">Title of Thread: *</label>
+                                <label for="title">Title of Thread: <span style="color:red">*</span> </label>
 
                                 <input type="text" class="form-control" id="title" placeholder="Title" name="title">
 
