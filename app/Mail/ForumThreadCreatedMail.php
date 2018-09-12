46026e4ed2e236dee1474dd46a33506077988f11
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2018-09-12 15:25:12 +0530

    Mail Class for threads and forums

---
+++ app/Mail/ForumThreadCreatedMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+
+class ForumThreadCreatedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+    public $link;
+    public $data;
+
+    public function __construct (User $user, $link, $data)
+    {
+        $this->user = $user;
+        $this->link = $link;
+        $this->data = $data;
+    }
+
+    /**
+     * [build description]
+     * @return [type] [description]
+     */
+    public function build()
+    {
+        return $this->markdown('emails.forumthreadcreated')->
+                      subject($this->data['subject']);
+    }
+}
+
+?>
