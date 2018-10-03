853f04f9d75ecfb568dcbffbb97652a2b437ce42
K Gautam <gautamv16@gmail.com>
2018-10-03 13:58:05 +0530

    fixes

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -648,7 +648,7 @@ class TopicController extends Controller {
 			 
 			 if(!$ifIamSingleSupporter) {
 			   $statement->go_live_time = strtotime(date('Y-m-d H:i:s', strtotime('+7 days')));
-			   $message ="Statement change submitted successfully. If no direct supporters object to this change, it will go live on ";
+			   $message ="Statement change submitted successfully.";
 			   $go_live_time = $statement->go_live_time;
 			 }
 			 
