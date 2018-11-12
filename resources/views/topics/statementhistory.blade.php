1e7db834f16f76757b258ebf83fac26e1ca8b88d
K Gautam <gautamv16@gmail.com>
2018-11-12 12:29:10 +0530

    fixed history issue

--- resources/views/topics/statementhistory.blade.php
+++ resources/views/topics/statementhistory.blade.php
@@ -105,7 +105,7 @@
 				    <a id="object" class="btn btn-historysmt" href="<?php echo url('manage/statement/'.$data->id.'-objection');?>">Object</a>
 				  <?php } ?>	
 					<a id="update" class="btn btn-historysmt" href="<?php echo url('manage/statement/'.$data->id);?>">Submit Statement Update Based On This</a>
-                    <a id="version" class="btn btn-historysmt" href="<?php echo url('topic/'.$data->topic_num.'/'.$data->camp_num.'?asof=bydate&asofdate='.date('Y/m/d H:i:s',$data->submit_time));?>">View This Version</a>
+                    <a id="version" class="btn btn-historysmt" href="<?php echo url('topic/'.$data->topic_num.'/'.$data->camp_num.'?asof=bydate&asofdate='.date('Y/m/d H:i:s',$data->go_live_time));?>">View This Version</a>
 				 
 				 </div>
 			    </div> 	
