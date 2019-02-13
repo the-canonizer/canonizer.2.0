e2d45267e3bc82fbcc9ae1b8d871c7d0e2d6b500
K Gautam <gautamv16@gmail.com>
2019-02-13 23:13:38 +0530

    topic create fixed

--- routes/web.php
+++ routes/web.php
@@ -66,19 +66,18 @@ Route::get('resetpassword/{token}', 'Auth\ResetPasswordController@showResetForm'
 Route::post('reset', 'Auth\ResetPasswordController@reset');
 Route::get('topic/{id}/{campnum}', [ 'as' => 'topic', 'uses' => 'TopicController@show']);
 Route::get('topic.asp/{id}/{campnum}', [ 'as' => 'topic', 'uses' => 'TopicController@show']);
+Route::get('topic/{id}', [ 'as' => 'topic', 'uses' => 'TopicController@show']);
 Route::post('loadtopic', 'HomeController@loadtopic');
 Route::get('camp/history/{id}/{campnum}', 'TopicController@camp_history');
 Route::get('statement/history/{id}/{campnum}', 'TopicController@statement_history');
 Route::get('topic-history/{id}', 'TopicController@topic_history');
 Route::get('api/v1/getcampoutline/{topic_num}/{camp_num}', 'ApiController@getcampoutline');
 Route::get('user/supports/{user_id}', 'TopicController@usersupports');
-Route::get('topic/{id}', [ 'as' => 'topic', 'uses' => 'TopicController@show']);
-Route::get('topic.asp/{id}', [ 'as' => 'topic', 'uses' => 'TopicController@show']);
 
 Route::group([ 'middleware' => 'auth'], function() {
     Route::resource('topic', 'TopicController');
     Route::get('camp/create/{topicnum}/{campnum}', [ 'as' => 'camp.create', 'uses' => 'TopicController@create_camp']);
-	Route::get('topic/create', [ 'as' => 'topic.create', 'uses' => 'TopicController@create']);
+	Route::get('create/topic', [ 'as' => 'topic.create', 'uses' => 'TopicController@create']);
     Route::post('camp/save', [ 'as' => 'camp.save', 'uses' => 'TopicController@store_camp']);
     Route::post('statement/save', [ 'as' => 'statement.save', 'uses' => 'TopicController@store_statement']);
     Route::get('settings', [ 'as' => 'settings', 'uses' => 'SettingsController@index']);
@@ -112,6 +111,8 @@ Route::group([ 'middleware' => 'auth'], function() {
      Route::post('/newsfeed/update',['as'=>'newsfeed.update','uses'=>'NewsFeedController@update']);
      
 });
+
+Route::get('topic.asp/{id}', [ 'as' => 'topic', 'uses' => 'TopicController@show']);
 
 Route::get(
         '/forum/{topicid}-{topicname}/{campnum}/threads', ['uses' => 'CThreadsController@index']
