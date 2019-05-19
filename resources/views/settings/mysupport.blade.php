8ea69689ccce66c164f84a8053c61ef04e538601
akuks <kukreti.ashutosh@gmail.com>
2019-05-19 19:28:06 +0530

    Fix for #503

--- resources/views/settings/mysupport.blade.php
+++ resources/views/settings/mysupport.blade.php
@@ -2,17 +2,17 @@
 @section('content')
 <div class="page-titlePnl">
     <h1 class="page-title">Supported Camps</h1>
-</div> 
+</div>
 
 @if(Session::has('error'))
 <div class="alert alert-danger">
-    <strong>Error! </strong>{{ Session::get('error')}}    
+    <strong>Error! </strong>{{ Session::get('error')}}
 </div>
 @endif
 
 @if(Session::has('success'))
 <div class="alert alert-success">
-    <strong>Success! </strong>{{ Session::get('success')}}    
+    <strong>Success! </strong>{{ Session::get('success')}}
 </div>
 @endif
 
@@ -33,26 +33,26 @@
 		        <p style="margin-left: 15px;color:red">Note : To change support order of camp, drag & drop the camp box on your choice position. </p>
                 @if(count($supportedTopic))
                  @foreach($supportedTopic as $data)
-                   
-                       <div class="SpCmpHd"><b>For Topic : {{ $data->topic->topic_name}}</b></div>
+
+                       <div class="SpCmpHd"><b>For Topic : <a href="/topic/{{ $data->topic->topic_num }}-{{ $data->topic->topic_name}}/">{{ $data->topic->topic_name}} </a> </b></div>
                		<div class="row column{{ $data->topic_num }}">
 					   <?php $topicSupport = $data->topic->Getsupports($data->topic_num,$userNickname);?>
 					   @foreach($topicSupport as $k=>$support)
-					    
+
 					   <div id="positions_{{ $support->support_id }}" class="SpCmpBDY support-sorter-element ui-widget ui-widget-content ui-helper-clearfix ui-corner-all">
 					     <form onsubmit="return confirm('Do you really want to delete this support ?');" action="{{ route('settings.support.delete')}}" id="support-{{$support->support_id}}" method="post">
 						    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-							
+
 							<input type="hidden" id="support_id_{{ $support->support_id }}" name="support_id" value="{{ $support->support_id }}">
 							<input type="hidden" id= "topic_num_{{ $support->support_id }}" name="topic_num" value="{{ $data->topic_num }}">
 							<input type="hidden" id= "nick_name_id_{{ $support->support_id }}" name="nick_name_id" value="{{ $support->nick_name_id }}">
 						  <button type="submit" id="submit_{{ $support->support_id }}" class="btn-sptclose" title="Remove Support"><i class="fa fa-close"></i></button>
-						 </form> 
-					     <b><span class="support_order">{{ $support->support_order }}</span> . {{ $support->camp->camp_name }} <br/>
+						 </form>
+					     <b><span class="support_order">{{ $support->support_order }}</span> . <a style="text-decoration: underline; color: blue;" href="/topic/{{ $data->topic_num }}-{{ $data->topic->topic_name }}/{{ $support->camp->camp_num }}"> {{ $support->camp->camp_name }}  </a> <br/>
 					   	 </b>
                        </div>
 					   @endforeach
-                    </div>   
+                    </div>
                     <script>
                     $( function() {
                         $( ".column{{ $data->topic_num }}" ).sortable({
@@ -64,26 +64,26 @@
                                     if(!data.success) {
                                         alert('Whoops, something went wrong :/');
                                     }
-                                    
+
                             }, 'json');
                             $( ".column{{ $data->topic_num }}" ).find('.support-sorter-element').each(function(i,v){
                                 $(v).find('.support_order').text(i+1);
                             });
-                            } 
+                            }
                         });
 
-                        
+
                     });
                     </script>
-					   
+
                  @endforeach
                @else
 				  <h6 style="margin-top:30px;margin-left:20px;"> You currently don't support any camps.</h6>
-               @endif			  
+               @endif
 
          </div>
-        
-    </div>   
+
+    </div>
  </div></div>
 </div>  <!-- /.right-whitePnl-->
 
@@ -98,4 +98,3 @@
 
 
     @endsection
-
