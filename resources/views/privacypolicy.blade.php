aac0c3bab15c9d161042cf760678a46019e1219a
K Gautam <gautamv16@gmail.com>
2019-08-08 23:19:14 +0530

    added terms and privacy page

---
+++ resources/views/privacypolicy.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('content')
+
+
+
+<div class="right-whitePnl">
+    <div class="container-fluid">
+        <h5>Canonizer Privacy Policy.</h5>
+
+
+       <p> You may read any Canonizer page without registering an account.</p>
+
+
+        <p>To modify a Canonizer page, you must provide a valid email address and register an account.</p>
+         
+        <p>Multiple accounts violate Canonizer’s one person/one vote principle and are not acceptable. Use of “sock puppets” may result in a temporary or permanent ban.</p>
+         
+        <p>Canonizer collects some information when you:</p>
+        <ul type="disc">
+          <li>Make public contributions.</li>
+          <li>Register an account or update your user page.</li>
+          <li>Use the Canonizer site.</li>
+          <li>Send us emails or participate in a survey or give feedback.</li>
+        </ul>
+        <p>We are committed to:</p>
+        <ul type="disc">
+            <li>Describing how your information may be used or shared in this Privacy Policy.</li>
+            <li>Using reasonable measures to keep your information secure.</li>
+            <li>Never selling your information or sharing it with third parties for marketing purposes.</li>
+            <li>Only sharing your information in limited circumstances, such as to improve Canonizer, to comply with the law, or to protect you and others.</li>
+            <li>Retaining your data for the shortest possible time that is consistent with maintaining, understanding, and improving Canonizer sites, and our obligations under law.</li>
+        </ul>
+
+        <p>Be aware:</p>
+        <ul type="disc">
+            <li>Any content you add or any change that you make to a Canonizer page will be publicly and permanently available.</li>
+            <li>Our community of volunteer editors and contributors is a self-policing and self-censoring body.</li>
+            <li>All users have access to propose edits and change to all Canonizer topic records, camp records, and camp statements.</li>
+            <li>Proposed changes will only be accepted if there is unanimous consent from the supporters of that camp.</li>
+            <li>For the protection of Canonizer.com and other users, if you do not agree with this Privacy Policy, you may not use <a href="{{url('home')}}">canonizer.com.</a></li>
+      </ul>
+
+
+    </div>
+    <!-- /.container-fluid-->
+</div>  <!-- /.right-whitePnl-->
+
+@endsection
---
+++ resources/views/termservice.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+
+
+
+<div class="right-whitePnl">
+    <div class="container-fluid">
+        <h5>Terms of Service</h5>
+
+        <p>You are free to:</p>
+        <ul type="disc">
+          <li><b>Read and Print</b> our articles and other media free of charge.</li>
+          <li><b>Share and Reuse</b> our articles and other media under free and open licenses.</li>
+          <li><b>Contribute To and Edit</b> our pages and projects..</li>
+         </ul>
+        <h6>Under the following conditions:</h6>
+        <ul type="disc">
+          <li><b>Responsibility –</b> You take responsibility for your edits (since we only host your content).</li>
+          <li><b>Civility –</b> You support a civil environment and do not harass other users.</li>
+          <li><b>Lawful Behavior – </b>You do not violate copyright or other laws.</li>
+          <li><b>No Harm –</b> You do not harm our technology infrastructure.</li>
+          <li><b>Terms of Use and Policies –</b> You adhere to the Terms of Use and to the applicable community policies when you visit Canonizer or participate in our communities.</li>
+        </ul> 
+        
+        <h6>With the understanding that:</h6>
+        <ul type="disc">
+          <li><b>You License Freely Your Contributions –</b> you generally must license your contributions and edits to our sites or Projects under a free and open license (unless your contribution is in the public domain).</li>
+          <li><b>No Professional Advice –</b> the content of articles and other projects is for informational purposes only and does not constitute professional advice.</li>
+      </ul>
+
+    </div>
+    <!-- /.container-fluid-->
+</div>  <!-- /.right-whitePnl-->
+
+@endsection
