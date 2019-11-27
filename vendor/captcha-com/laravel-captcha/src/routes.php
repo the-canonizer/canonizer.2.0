7b2b96c6acb216c09ca7f0ebfce2efc0e2b12047
K Gautam <gautamv16@gmail.com>
2019-11-28 01:22:11 +0530

    changes

--- vendor/captcha-com/laravel-captcha/src/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Route::get('captcha-handler', 'LaravelCaptcha\Controllers\CaptchaHandlerController@index');
-Route::get('simple-captcha-handler', 'LaravelCaptcha\Controllers\SimpleCaptchaHandlerController@index');
