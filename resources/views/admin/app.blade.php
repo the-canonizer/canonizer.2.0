31879d85624ff309152e50613cdcd9d80f923555
K Gautam <gautamv16@gmail.com>
2019-11-28 22:40:16 +0530

    changes

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -45,10 +45,10 @@
 					          <li class ="{{ ($route=='getIndex') ? 'current':''}}"><a href="{{ url('/admin') }}"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
                     <li class="{{ ($route=='namespace') ? 'current':''}}"><a href="{{ url('/admin/namespace') }}"><i class="glyphicon glyphicon-home"></i> Namespace</a></li>
                     <li class="{{ ($route=='getNamespaceRequests') ? 'current':''}}"><a href="{{ url('/admin/namespace-requests') }}"><i class="glyphicon glyphicon-calendar"></i> Namespace Requests</a></li>
-                    <li class="{{ ($route=='getIndex') ? 'current':''}}"><a href="{{ url('/admin/users') }}"><i class="glyphicon glyphicon-calendar"></i> Users</a></li>
-                    <li class="{{ ($route=='index') ? 'current':''}}"><a href="{{ url('/admin/templates') }}"><i class="glyphicon glyphicon-pencil"></i> Templates</a></li>
+                    <li class="{{ ($route=='getIndex' & str_contains(Request::fullUrl(), 'users')) ? 'current':''}}"><a href="{{ url('/admin/users') }}"><i class="glyphicon glyphicon-calendar"></i> Users</a></li>
+                    <li class="{{ ($route=='index' & str_contains(Request::fullUrl(), 'templates')) ? 'current':''}}"><a href="{{ url('/admin/templates') }}"><i class="glyphicon glyphicon-pencil"></i> Templates</a></li>
                     <li class="{{ ($route=='getSendmail') ? 'current':''}}"><a href="{{ url('/admin/sendmail') }}"><i class="glyphicon glyphicon-pencil"></i> Send Email</a></li>
-                    <li class="{{ ($route=='index') ? 'current':''}}"><a href="{{ url('/admin/videopodcast') }}"><i class="glyphicon glyphicon-facetime-video"></i> Video Podcast</a></li>
+                    <li class="{{ ($route=='index' & str_contains(Request::fullUrl(), 'videopodcast')) ? 'current':''}}"><a href="{{ url('/admin/videopodcast') }}"><i class="glyphicon glyphicon-facetime-video"></i> Video Podcast</a></li>
                     <!--<li><a href="stats.html"><i class="glyphicon glyphicon-stats"></i> Statistics (Charts)</a></li>
                     <li><a href="tables.html"><i class="glyphicon glyphicon-list"></i> Tables</a></li>
                     <li><a href="buttons.html"><i class="glyphicon glyphicon-record"></i> Buttons</a></li>
