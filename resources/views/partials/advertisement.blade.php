450f694bf4b22bd3f9e6614a9443c5a855038d6e
K Gautam <gautamv16@gmail.com>
2019-12-28 15:29:09 +0530

    changes

--- resources/views/partials/advertisement.blade.php
+++ resources/views/partials/advertisement.blade.php
@@ -1,6 +1,6 @@
 <div class="right-addsense">
    
-	<script async type="text/javascript">
+	<script type="text/javascript">
 	  
             google_ad_client = "pub-6646446076038181";
             //120x600, right bar
@@ -9,6 +9,16 @@
             google_ad_height = 600;
             //-->
 	</script>
-          <script async  type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+    <!-- <script async  type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script> -->
+    <script>
+              (function () {
+                var script = document.createElement(s); 
+                script.type = 'text/javascript'; 
+                script.async = true;
+                script.src = 'pagead2.googlesyndication.com/pagead/show_ads.js';
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(script, s);
+              })();
+</script>
             
 </div> 
