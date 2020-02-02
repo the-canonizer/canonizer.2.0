071169a01fa0906b67113a175e8149dc4b92622d
K Gautam <gautamv16@gmail.com>
2020-02-02 23:41:55 +0530

    changes

--- resources/views/topics/statementhistory.blade.php
+++ resources/views/topics/statementhistory.blade.php
@@ -191,7 +191,9 @@
 				  
 				 <div class="CmpHistoryPnl-footer">
 				  <?php if($currentTime < $data->go_live_time && $currentTime >= $data->submit_time && $ifSupportingThisCampOrChild) { ?>
+            @if($isagreeFlag && $ifIamSupporter && Auth::user()->id != $submitterUserID)
 				    <a id="object" class="btn btn-historysmt" href="<?php echo url('manage/statement/'.$data->id.'-objection');?>">Object</a>
+            @endif
 				  <?php } ?>	
 					<a id="update" class="btn btn-historysmt" href="<?php echo url('manage/statement/'.$data->id);?>">Submit Statement Update Based On This</a>
                     <a id="version" class="btn btn-historysmt" href="<?php echo url('topic/'.$data->topic_num.'/'.$data->camp_num.'?asof=bydate&asofdate='. date('Y/m/d H:i:s', $data->go_live_time)); ?>">View This Version</a>
