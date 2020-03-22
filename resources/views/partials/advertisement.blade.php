ea753f387c4aad823a53561f246e800db2918dcf
K Gautam <gautamv16@gmail.com>
2020-03-22 23:49:36 +0530

    changes

--- resources/views/partials/advertisement.blade.php
+++ resources/views/partials/advertisement.blade.php
@@ -5,6 +5,15 @@
             google_ad_slot = "5819006657";
             google_ad_width = 115;
             google_ad_height = 600;
+            var link = document.createElement('link');
+            link.rel = 'import';
+            link.onload = function() {
+                var link = document.querySelector('link[rel=import]');
+                var content = link.import.querySelector('#imported-content');
+                document.getElementById('import-container').appendChild(content.cloneNode(true));
+            };
+            link.href = "//pagead2.googlesyndication.com/pagead/show_ads.js";
+            document.getElementsByClassName('right-addsense')[0].appendChild(link);
             </script>
-            <script type="text/javascript"  src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+            <!-- <script type="text/javascript"  src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script> -->
 </div> 
