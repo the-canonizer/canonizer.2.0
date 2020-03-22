a63f146d44fc1bb4a4f22ab5fe127895b849072b
K Gautam <gautamv16@gmail.com>
2020-03-22 23:29:24 +0530

    changes

--- resources/views/partials/advertisement.blade.php
+++ resources/views/partials/advertisement.blade.php
@@ -1,19 +1,19 @@
 
 <div id="google_ads" class="right-addsense">
     <script type="text/javascript">
-            google_ad_client = "pub-6646446076038181";
-            google_ad_slot = "5819006657";
-            google_ad_width = 115;
-            google_ad_height = 600;
-            function loadGoogleAds() {
+            window.onload = function(){
+                     google_ad_client = "pub-6646446076038181";
+                    google_ad_slot = "5819006657";
+                    google_ad_width = 115;
+                    google_ad_height = 600;
                     var scriptTag = document.createElement('script');
                     scriptTag.setAttribute('src', '//pagead2.googlesyndication.com/pagead/show_ads.js');
                     scriptTag.setAttribute('type', 'text/javascript');
                     scriptTag.setAttribute('async', 'async');
                     document.getElementById('google_ads').appendChild(scriptTag);
-                   // document.body.appendChild(scriptTag);
-                }
-                window.addEventListener("load", loadGoogleAds);
+            };
+           
+          
             </script>
             <!-- <script type="text/javascript"  src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script> -->
 </div> 
