cfbed3d754dca39ca6222e97fae5b1c3bb77be29
K Gautam <gautamv16@gmail.com>
2020-03-22 23:33:31 +0530

    changes

--- resources/views/partials/advertisement.blade.php
+++ resources/views/partials/advertisement.blade.php
@@ -1,19 +1,10 @@
 
-<div id="google_ads" class="right-addsense">
+<div class="right-addsense">
     <script type="text/javascript">
-            window.onload = function(){
-                     google_ad_client = "pub-6646446076038181";
-                    google_ad_slot = "5819006657";
-                    google_ad_width = 115;
-                    google_ad_height = 600;
-                    var scriptTag = document.createElement('script');
-                    scriptTag.setAttribute('src', '//pagead2.googlesyndication.com/pagead/show_ads.js');
-                    scriptTag.setAttribute('type', 'text/javascript');
-                    //scriptTag.setAttribute('async', 'async');
-                    document.getElementById('google_ads').appendChild(scriptTag);
-            };
-           
-          
+            google_ad_client = "pub-6646446076038181";
+            google_ad_slot = "5819006657";
+            google_ad_width = 115;
+            google_ad_height = 600;
             </script>
-            <!-- <script type="text/javascript"  src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script> -->
+            <script type="text/javascript"  src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 </div> 
