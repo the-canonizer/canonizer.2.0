fe5af4cc8519bf6f839b91b48eaab45b19b31e41
K Gautam <gautamv16@gmail.com>
2020-04-05 23:18:29 +0530

    changes

--- resources/views/emails/forumpostsubmitemail.blade.php
+++ resources/views/emails/forumpostsubmitemail.blade.php
@@ -14,27 +14,24 @@
 
 <p> {!! $data['post'] !!}. </p>
 
-
+<p>
 @if(isset($data['subscriber']) && $data['subscriber'] == 1)
 <h4>You are receiving this e-mail because:</h4>
-<p>
 	<ul>
 	<li>You are subscribed to <a href="{{ url('/').'/'.$data['camp_url'] }}"> {{ $data['camp_name'] }} </a></li>
 </ul>
-</p>
 @else
 	<h4>You are receiving this e-mail because:</h4>
-	<p>
 		<ul>
 			<li>You are directly supporting <a href="{{ url('/').'/'.$data['camp_url'] }}"> {{ $data['camp_name'] }} </a></li>
 		</ul>
-	</p>
+
 	<h4>Note:</h4>
 	<p>
 	 We request that all <b>direct</b> supporters of a camp continue to receive notifications and take responsibility for the camp. You can avoid being notified by <b>delegating</b> your support to someone else.
 	</p>
 @endif
-
+</p>
 <p>  Sincerely, </p>
 <p> {{ config('app.email_signature') }}</p>
 @endcomponent
