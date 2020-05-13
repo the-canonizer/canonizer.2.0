9d084660a701ef13cc71e792576e3600f8ca5fd9
K Gautam <gautamv16@gmail.com>
2020-05-14 00:02:33 +0530

    changes

--- config/app.php
+++ config/app.php
@@ -21,7 +21,7 @@ return [
     'front_page_limit'=>env('FRONT_PAGE_LIMIT', '30'),
     'go_live_day_limit'=>1,
     'go_live_text' => 'one day / 24 hours',
-    'mail_env' => '[staging.canon]',
+    'mail_env' => '[canon]',
 
     /*
     |--------------------------------------------------------------------------
