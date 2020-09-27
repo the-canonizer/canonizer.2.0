db84576aa68d130de246bba3f22663bcf1c86581
Gautam <gautamv16@gmail.com>
2020-09-27 23:30:00 +0530

    changes

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -197,7 +197,7 @@
                                 <label for="address_1">Address Line 1 (Limit 255 Chars) </label>
 								</br>
 								<div style="width:300px;float:left">
-                                <input type="text" onkeydown="restrictTextField(event,255)" name="address_1" class="form-control" id="address_1" value="{{ old('address_1', $user->address_1)}}">
+                                <input type="text" onkeydown="restrictTextField(event,255)" onfocus="initAutocomplete()" name="address_1" class="form-control" id="address_1" value="{{ old('address_1', $user->address_1)}}">
                                 </div>
 								<div style="width:95px;float:right">
 								<select class="form-control" id="address_1_bit"  name="address_1_bit">
@@ -299,8 +299,71 @@
     </div>   
  </div></div>
 </div>  <!-- /.right-whitePnl-->
-
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMtlWJeoKZiwNw9FXG-xLTKj7GvkonarA&libraries=places"></script>
     <script>
+    	let placeSearch;
+		let autocomplete;
+		const componentForm = {
+		  street_number: "short_name",
+		  route: "long_name",
+		  locality: "long_name",
+		  administrative_area_level_1: "short_name",
+		  country: "long_name",
+		  postal_code: "short_name",
+		};
+		function initAutocomplete() {
+  		// Create the autocomplete object, restricting the search predictions to
+  		// geographical location types.
+	  autocomplete = new google.maps.places.Autocomplete(
+	    document.getElementById("address_1"),
+	    { types: ["geocode"] }
+	  );
+	  // Avoid paying for data that you don't need by restricting the set of
+	  // place fields that are returned to just the address components.
+	  autocomplete.setFields(["address_component"]);
+	  // When the user selects an address from the drop-down, populate the
+	  // address fields in the form.
+	  autocomplete.addListener("place_changed", fillInAddress);
+	}
+
+	function fillInAddress() {
+	  // Get the place details from the autocomplete object.
+	  const place = autocomplete.getPlace();
+	  console.log('place',place);
+	  for (const component in componentForm) {
+	    document.getElementById(component).value = "";
+	    document.getElementById(component).disabled = false;
+	  }
+
+	  // Get each component of the address from the place details,
+	  // and then fill-in the corresponding field on the form.
+	  for (const component of place.address_components) {
+	    const addressType = component.types[0];
+
+	    if (componentForm[addressType]) {
+	      const val = component[componentForm[addressType]];
+	      document.getElementById(addressType).value = val;
+	    }
+	  }
+	}
+
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	function geolocate() {
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition((position) => {
+	      const geolocation = {
+	        lat: position.coords.latitude,
+	        lng: position.coords.longitude,
+	      };
+	      const circle = new google.maps.Circle({
+	        center: geolocation,
+	        radius: position.coords.accuracy,
+	      });
+	      autocomplete.setBounds(circle.getBounds());
+	    });
+	  }
+	}
         $(document).ready(function () {
             $("#birthday").datepicker({
                 changeMonth: true,
