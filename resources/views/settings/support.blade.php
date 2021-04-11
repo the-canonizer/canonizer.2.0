10a41da588e20ec24fe8191bd6a6e453c2164467
Gautam <gautamv16@gmail.com>
2021-04-11 23:48:10 +0530

    changes

--- resources/views/settings/support.blade.php
+++ resources/views/settings/support.blade.php
@@ -107,10 +107,11 @@
 					
 					 <div class="col-sm-6">
 					<div class="row column">
-					<?php 
+						
+					<?php echo "<pre>"; print_r($supportedTopic->topic->Getsupports($supportedTopic->topic_num,[$supportedTopic->nick_name_id])); die;
 					$k = 0; $topicSupportCamp = $supportedTopic->topic->Getsupports($supportedTopic->topic_num,[$supportedTopic->nick_name_id]);
 
-				echo "<pre>"; print_r($supportedTopic->topic->Getsupports($supportedTopic->topic_num,[$supportedTopic->nick_name_id])); die;
+				
 					?>
 
                        
