ea5b1cd2f0853d90964386e41eae0c4f8c6eb8d1
brent.allsop <brent.allsop@canonizer.com>
2021-04-21 11:10:20 -0600

    Change the "What is Canonizer.com? on the main welcome  page to "Help us bring the world together!"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
 <div class="right-whitePnl">
     <div class="container-fluid">
         <div class="Gcolor-Pnl">
-            <h3>What is Canonizer.com?</h3>
+            <h3>Help us bring the world together!</h3>
             <div class="content">
 
                 <iframe src="https://player.vimeo.com/video/307590745" style="position: relative;width: 100%;height:400px;border:none;" id="homeiframe">
