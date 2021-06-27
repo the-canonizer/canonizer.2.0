4e35dbae323ccf7a82f3eb039a5dcdcf425c735a
Gautam <gautamv16@gmail.com>
2021-06-27 16:29:06 +0530

    changes

--- resources/views/topics/statementhistory.blade.php
+++ resources/views/topics/statementhistory.blade.php
@@ -230,12 +230,14 @@
                                  @if($isagreeFlag && $ifIamSupporter   && Auth::user()->id != $submitterUserID)
                                 <div class="CmpHistoryPnl-footer">
                                     <div>
-                                       <input {{ (isset($isAgreed) && $isAgreed) ? 'checked' : '' }} {{ (isset($isAgreed) && $isAgreed) ? 'disabled' : '' }} class="agree-to-change" type="checkbox" name="agree" value="" onchange="agreeToChannge(this,'{{ $data->id}}')"> I agree with this statement change</form>
+                                        @if($stmentLength ==1 || ($stmentLength >1 &&  $liveStatement && $data->submit_time  > $liveStatement->submit_time))
+                                       <input {{ (isset($isAgreed) && $isAgreed) ? 'checked' : '' }} {{ (isset($isAgreed) && $isAgreed) ? 'disabled' : '' }} class="agree-to-change" type="checkbox" name="agree" value="" onchange="agreeToChannge(this,'{{ $data->id}}')"> I agree with this statement change</input>
+                                       @endif
                                     </div>
                                 </div>
                                 @endif
 
-                                  @if(Auth::user()->id == $submitterUserID && $isGraceFlag  && $data->grace_period && $interval > 0)
+                                  @if(Auth::user()->id == $submitterUserID && $isGraceFlag   && $data->grace_period && $interval > 0)
                                     <div class="CmpHistoryPnl-footer" id="countdowntimer_block<?php echo $data->id ;?>">
                                         <div class="grace-period-note"><b>Note: </b>This countdown timer is the grace period in which you can make minor changes to your statement before other direct supporters are notified.</div>
                                         <div style="float: right" > 
