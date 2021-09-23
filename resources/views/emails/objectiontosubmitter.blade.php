c6e520d1466550793e3f752f2603042b488b3b4f
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2021-09-23 23:15:47 +0530

    Removed php code from the blade

--- resources/views/emails/objectiontosubmitter.blade.php
+++ resources/views/emails/objectiontosubmitter.blade.php
@@ -1,9 +1,6 @@
-@php
-    $link = url('/') . '/' . $link;
-@endphp
 @component('mail::message')
  Hello {{ $user->first_name }} {{ $user->last_name }},<br/>
-<p>{{ $data['nick_name'] }} has objected to your <a href="{{$link}}" target='_balnk'>proposed change</a> submitted for {{$data['type']}} (<a href="{{$data['topic_link']}}">{{$data['object']}}</a>) {{$data['object_type']}} </p>
+<p>{{ $data['nick_name'] }} has objected to your <a href="{{ url('/') . '/' . $link }}" target='_balnk'>proposed change</a> submitted for {{$data['type']}} (<a href="{{$data['topic_link']}}">{{$data['object']}}</a>) {{$data['object_type']}} </p>
 @if(isset($data['help_link']))
 @component('mail::button', ['url' => url('/') . '/' . $data['help_link']])
 See this link for options you can take when there are objections
