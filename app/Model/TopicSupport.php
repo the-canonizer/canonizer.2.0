e4ab5852998e0f9481e7088b27f9d26540c8f04e
Gautam <gautamv16@gmail.com>
2021-10-11 16:15:54 +0530

    811 issue fixed

--- app/Model/TopicSupport.php
+++ app/Model/TopicSupport.php
@@ -171,25 +171,39 @@ class TopicSupport extends Model {
     public static function buildTree($topicnum,$campnum,$traversedTreeArray,$parentNode=false,$add_supporter = false){
         
         $html= "";
-
+        $userId = null;
+        if(Auth::check()){
+            $userId = Auth::user()->id;
+        }
+        // check if anyone is delegating to logged in user
+        $delegatingToCurrentUser = [];
+        $userNicknames = Nickname::personNicknameArray();
+        $myDelegator = Support::where('topic_num', $topicnum)->whereIn('delegate_nick_name_id', $userNicknames)->where('end', '=', 0)->groupBy('nick_name_id')->get();
+        if($myDelegator && count($myDelegator) > 0){
+            foreach($myDelegator as $delegator){
+                $delegatingToCurrentUser[$delegator->delegate_nick_name_id] =  $delegator->nick_name_id;
+            }            
+        }
         foreach($traversedTreeArray as $array){
             $nickName = Nickname::where('id',$array['index'])->first();
             $nickNameArr = $nickName ->personNicknameArray();
             $userFromNickname = $nickName->getUser();
             $delegatedUserID = [];
+            $delegatedUserID[] = $userFromNickname->id;
+            if($delegatingToCurrentUser && count($delegatingToCurrentUser) > 0 && in_array($array['index'],$delegatingToCurrentUser) ){
+                $delegatedUser = array_search($array['index'],$delegatingToCurrentUser);
+                $delnickName1 = Nickname::where('id',$delegatedUser)->first();
+                $deluserFromNickname1 = $delnickName1->getUser();
+                $delegatedUserID[] = $deluserFromNickname1->id;
+              }
             if(is_array($array['children']) && sizeof($array['children']) > 0){
                 foreach ($array['children'] as $key => $value) {
-                    $delnickName = Nickname::where('id',$value['index'])->first();
+                   $delnickName = Nickname::where('id',$value['index'])->first();
                    $deluserFromNickname = $delnickName->getUser();
-                   $delegatedUserID[] = $deluserFromNickname->id;
-                }
-                
-            }
-            $userId = null;
-            if(Auth::check()){
-                $userId = Auth::user()->id;
-            }
-            $topicData = Topic::getLiveTopic($topicnum,['nofilter'=>true]);//
+                   $delegatedUserID[] = $deluserFromNickname->id;                   
+                }                
+            }           
+            $topicData = Topic::getLiveTopic($topicnum,['nofilter'=>true]);
             $namespace_id = (isset($topicData->namespace_id)) ? $topicData->namespace_id:1;
             $supports = $nickName->getSupportCampList($namespace_id);
             $support_number = self::getSupportNumber($topicnum,$campnum,$supports);
@@ -200,17 +214,16 @@ class TopicSupport extends Model {
                     $space_html='<span class="" title="Collapse"></span>';
                 }
              if($parentNode){
-               
                 $html.= "<li class='main-parent'>".$space_html."<a href='".route('user_supports',$nickName->id)."?topicnum=".$topicnum."&campnum=".$campnum."&namespace=".$namespace_id."#camp_".$topicnum."_".$campnum."'>{$support_txt}{$nickName->nick_name}</a><div class='badge'>".round($array['score'],2)."</div>";
-                if($userId && $userFromNickname->id != $userId && !in_array($userId, $delegatedUserID) &&  isset($ifSupporter) && !$ifSupporter &&  !$add_supporter){
+                if( $userId && $userFromNickname->id != $userId && !in_array($userId, $delegatedUserID) &&  isset($ifSupporter) && !$ifSupporter &&  !$add_supporter){
                     $urlPortion = Camp::getSeoBasedUrlPortion($topicnum,$campnum);
                     $html.='<a href="'.url('support/'.$urlPortion.'_'.$array['index']).'" class="btn btn-info">Delegate Your Support</a>';
                 }
                 
             }else{
-               
                 $html.= "<li>".$space_html."<a href='".route('user_supports',$nickName->id)."?topicnum=".$topicnum."&campnum=".$campnum."&namespace=".$namespace_id."#camp_".$topicnum."_".$campnum."'>{$support_txt}{$nickName->nick_name}</a><div class='badge'>".round($array['score'],2)."</div> ";
-                 if($userId && $userFromNickname->id != $userId && !in_array($userId, $delegatedUserID) && isset($ifSupporter) && !$ifSupporter &&   !$add_supporter){
+               
+                if( $userId && $userFromNickname->id != $userId && !in_array($userId, $delegatedUserID) && isset($ifSupporter) && !$ifSupporter &&   !$add_supporter){
                     $urlPortion = Camp::getSeoBasedUrlPortion($topicnum,$campnum);
                      $html.='<a href="'.url('support/'.$urlPortion.'_'.$array['index']).'" class="btn btn-info">Delegate Your Support</a>';
                 }
@@ -218,8 +231,6 @@ class TopicSupport extends Model {
             $html.="<ul>";
             $html.=self::buildTree($topicnum,$campnum,$array['children'],false,$add_supporter);
             $html.="</ul></li>";
-            
-        
         }
         return $html;
     }
