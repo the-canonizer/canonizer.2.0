c6da48a66250d6d348f26c9e9dc4d999aa5188f8
Kshitij Singh <kshitij.singh@iffort.com>
2021-10-19 11:00:49 +0530

    Fixes #822 : Users are only able to register through email, but when logging in, the system says that a users can enter a phone number to login as well

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
     <form id="login_form" action="{{ url('/login')}}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group">
-            <label for="firstname">Enter Email / Phone Number <span style="color:red">*</span></label>
+            <label for="firstname">Enter Email <span style="color:red">*</span></label>
             <input id="email" type="text" name="email" class="form-control" id="email" value="{{ old('email')}}">
             @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
         </div>
