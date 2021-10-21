5253b0e2a37ae3d5fd26d0ab95136843e6e5d0c9
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-10-21 13:33:52 +0530

    Add diffForHumans() function to readable date format

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -68,7 +68,8 @@
               </a>
             </td>
             <td>{{ $thread->replies->count() }}</td>
-            <td>replied on {{ to_local_time($date)}}</td> <!-- By Reena Nalwa Talentelgia #780 -->
+            <td>
+              replied on {{ Carbon\Carbon::createFromTimestamp( $date )->diffForHumans() }} </td>
           </tr>
           @endforeach
         </tbody>
