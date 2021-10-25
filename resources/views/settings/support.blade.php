ff02ebf22b9d39864f3e0b69e3d65f2321cc47ad
ali ahmad <ali.ahmad@codedistrict.com>
2021-10-25 17:47:22 +0500

    ticket #781, #778

--- resources/views/settings/support.blade.php
+++ resources/views/settings/support.blade.php
@@ -96,7 +96,7 @@
                 <li ><a class="" href="{{ route('settings.blockchain')}}">Crypto Verification (was MetaMask Account)</a></li>
             </ul>
 		<form id="support_form" action="{{ route('settings.support.add')}}" method="post">	
-         <div class="SupportCmp">
+         <div class="SupportCmp" id="draggable-area">
 		        <p style="margin-left: 15px;color:red">Note : To change support order of camp, drag & drop the camp box on your choice position. </p>
 		        <?php $lastsupportOrder = 0;
 				
@@ -321,6 +321,9 @@ $('#widget').draggable();
                 cursor: 'move',
                 opacity: 0.6,
 				axis: "y",
+				scroll: false,
+				containment: "#draggable-area",
+				
                 update: function(event, ui) {
                 $( ".column" ).find('.support-sorter-element').each(function(i,v){
                         $(v).find('.support_order').text(i+1);
