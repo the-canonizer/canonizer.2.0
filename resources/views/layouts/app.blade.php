8769e2e86f91941f063084346584d290fc3562e0
gautam <gautamv16@gmail.com>
2021-10-28 19:28:08 +0530

    Revert "Revert "Iffort""
    This reverts commit fc1221e121cea65e1143f45ef6e5f7d0cc96b168.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,8 +84,8 @@
                         </div>
                         @else
 						<a class="nav-link guestLogin" style="cursor:default;">Browsing as: Guest</a>
-                        <a class="nav-link" href="{{ url('/login')}}"><i class="fa fa-fw fa-user"></i> Log in</a>
-                        <a class="nav-link" href="{{ url('/register')}}"><i class="fa fa-fw fa-user-plus"></i> Register </a>
+                        <a class="nav-link @if(Request::url() == url('/login')) active @endif" href="{{ url('/login')}}"><i class="fa fa-fw fa-user"></i> Log in</a>
+                        <a class="nav-link @if(Request::url() == url('/register')) active @endif" href="{{ url('/register')}}"><i class="fa fa-fw fa-user-plus"></i> Register </a>
                         @endif
                     </li>
                 </ul>
