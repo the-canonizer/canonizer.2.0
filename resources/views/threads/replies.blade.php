131ffe3dac5eb9b5323d4d033588705f0d4f2df3
developer <sunnyspr7@gmail.com>
2021-10-28 18:25:22 +0530

    #14

--- resources/views/threads/replies.blade.php
+++ resources/views/threads/replies.blade.php
@@ -15,7 +15,12 @@
             </a>
                 replied {{ Carbon\Carbon::createFromTimestamp( $reply->updated_at )->diffForHumans() }}
                 ({{ to_local_time( $reply->updated_at )  }})
-            <br><br>
+                @if($userNicknames[0]->id==$reply->user_id) 
+                    <a href="{{ URL::to('/')}}/forum/{{ $topicname }}/{{ $campnum }}/threads/{{ $reply->c_thread_id }}-{{ $reply->id }}">
+                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </a>
+                @endif
+               <br><br>
         </div>
 
     </div>
