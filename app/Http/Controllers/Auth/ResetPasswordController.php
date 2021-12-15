594b1c9982c32104933b729decc45eb61a64b3c8
Kshitij Singh <kshitij.singh@iffort.com>
2021-12-15 14:35:13 +0530

    #984: Added migration and model for forgot password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -44,6 +44,7 @@ class ResetPasswordController extends Controller {
     }
 
     public function showResetForm(Request $request, $token = null) {
+        
         return view('auth.passwords.reset')->with(
                         ['email' => base64_decode($token)]
         );
