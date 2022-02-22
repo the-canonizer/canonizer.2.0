afecfc743de241e8428bb5f79ad3cb5070baaf3f
Karun <karun.gautam0607@gmail.com>
2022-02-22 19:31:03 +0530

    fixed issue #1155

--- app/Http/Controllers/Admin/ActionController.php
+++ app/Http/Controllers/Admin/ActionController.php
@@ -23,7 +23,7 @@ class ActionController extends Controller
 			
 			  $mysqldump =  (stristr(PHP_OS, 'WIN')) ? exec("where mysqldump") : exec("which mysqldump");
    			try{
-				 $command = "$mysqldump --column-statistics=0  -P $dbport -h $dbhost -u$dbuser '-p$dbpass' $dbname > $dbexportPath";
+				 $command = "$mysqldump  -P $dbport -h $dbhost -u$dbuser '-p$dbpass' $dbname > $dbexportPath";
 				 $output=array();
 				exec($command,$output,$worked);
 			}catch(\Exception $e){
