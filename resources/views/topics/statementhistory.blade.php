41b61207524333f5a30f47ce3a0dcc849dfa39fb
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-03-09 16:19:54 +0530

    Fix #1203 - Delegated supporter is able to object topic/camp/statement change

--- resources/views/topics/statementhistory.blade.php
+++ resources/views/topics/statementhistory.blade.php
@@ -214,7 +214,7 @@
                   @endif 
 				  
 				 <div class="CmpHistoryPnl-footer">
-				    <?php if($currentTime < $data->go_live_time && $currentTime >= $data->submit_time && ($ifIamSupporter || $ifIamDelegatedSupporter)) { ?>
+				    <?php if($currentTime < $data->go_live_time && $currentTime >= $data->submit_time && ($ifIamSupporter)) { ?>
                         <a id="object" class="btn btn-historysmt mb-1" href="<?php echo url('manage/statement/'.$data->id.'-objection');?>">Object</a>
                     <?php }else if($currentTime < $data->go_live_time && $currentTime >= $data->submit_time && $ifSupportDelayed){ ?>
                         <button type="button" onClick="disagreementPopup()" class="btn btn-historysmt mb-1 disable-btn">Object &nbsp;<i title="You can not object to this camp statement." class="fa fa-info-circle" aria-hidden="true"></i></button>
