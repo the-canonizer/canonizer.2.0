c754a077c4668adcf3c02195c42c6183a6217e03
ahmed130544 <muhammad.ahmed@codedistrict.com>
2022-04-15 11:56:05 +0500

    Update support.blade.php

--- resources/views/settings/support.blade.php
+++ resources/views/settings/support.blade.php
@@ -166,8 +166,9 @@
 						<?php 
 							$liveCamp = \App\Model\Camp::getLiveCamp($support->topic_num,$support->camp_num,['nofilter'=>true]);
 							$url =  \App\Model\Camp::getTopicCampUrl($support->topic_num,$support->camp_num);
+							$key = $key + 1;
 						?>
-						@if(!in_array($support->camp->camp_num,$removedCampList)) <?php $key = $key + 1; ?>
+						@if(!in_array($support->camp->camp_num,$removedCampList)) <?php  ?>
 						
 						<div class="col-sm-12 column sortCamp">
 							<div id="positions_{{ $support->support_id }}" class="SpCmpBDY  support-sorter-element ui-widget ui-widget-content ui-helper-clearfix ui-corner-all">
@@ -187,7 +188,9 @@
 							</div>
 						</div>
 						@else
+							
 							@if(Session::get('confirm') !='samecamp' && !Session::has('warningDelegate')) 
+							
 								<!-- CASE: When adding support to new camp which is a sub-camp of a specific camp  --> 
 								<?php $lastsupportOrder++;
 									$isChildCamp = 1;
@@ -196,13 +199,13 @@
 								<div class="col-sm-12 column sortCamp">   
 									<div id="positions_0" class="SpCmpBDY  support-sorter-element ui-widget ui-widget-content ui-helper-clearfix ui-corner-all">
 										
-										<input type="hidden" class="final_support_order" name="support_order[{{$camp->camp_num}}]" id="support_order_0" value="{{ $key + 1  }}">
+										<input type="hidden" class="final_support_order" name="support_order[{{$camp->camp_num}}]" id="support_order_0" value="{{ $key  }}">
 										
 										<input type="hidden" name="camp[{{$camp->camp_num}}]" value="{{ $camp->camp_num }}">
 										<input type="hidden" name="delegated[{{$camp->camp_num}}]" value="{{ $delegate_nick_name_id }}">
 										
-										<b><a class="mr-5" href="<?= $url; ?>"><span class="support_order">{{ $key+1}} </span> . {{ $camp->camp_name }} </a></b><br/>
-										<span rel="{{$camp->camp_num}}" order="{{ $key + 1  }}" delegated="{{ $delegate_nick_name_id }}" class="x-btn remove_camp">X</span>                        
+										<b><a class="mr-5" href="<?= $url; ?>"><span class="support_order">{{ $key}} </span> . {{ $camp->camp_name }} </a></b><br/>
+										<span rel="{{$camp->camp_num}}" order="{{ $key }}" delegated="{{ $delegate_nick_name_id }}" class="x-btn remove_camp">X</span>                        
 									</div>
 								</div>	
 							@endif
