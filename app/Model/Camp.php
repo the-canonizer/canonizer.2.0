ec3690663d2de1c36f45eba011d9710aca9429a4
Karun <karun.gautam0607@gmail.com>
2022-05-08 22:43:37 +0530

    changes for peer algo

--- app/Model/Camp.php
+++ app/Model/Camp.php
@@ -1026,7 +1026,7 @@ class Camp extends Model {
 
         $reducedTree = Util::execute('POST', $endpoint, $headers, $requestBody);
 
-        $data = json_decode($reducedTree, true);
+        $data = [];//json_decode($reducedTree, true);
         
         if(count($data['data']) && $data['code'] == 200 ){
             $reducedTree = $data['data'][0];
