a62f31ec5df1947606e9fe071ea69e7da561ab5a
Karun <karun.gautam0607@gmail.com>
2022-07-22 16:55:54 +0530

    debugging address api

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -604,7 +604,8 @@
 
 	function fillInAddress() {
 	  document.getElementById('address_1').value = "";
-	  const place = autocomplete.getPlace();	  
+	  const place = autocomplete.getPlace();
+      console.log('place',place);	  
 	  ifAddressSelected = true;
 	  if(place && place.address_components)
 	  for (const component of place.address_components) {
