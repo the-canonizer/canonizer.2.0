96dd111c4ebd5ccc27b016600d74adec63eaca0e
Karun <karun.gautam0607@gmail.com>
2022-08-28 13:58:26 +0530

    added terms and service updated page

--- resources/views/termservice.blade.php
+++ resources/views/termservice.blade.php
@@ -1,32 +1,54 @@
 @extends('layouts.app')
 @section('content')
-
-
-
-<div class="right-whitePnl">
+<div class="right-whitePnl termsnService">
     <div class="container-fluid">
-        <h5>Terms of Service</h5>
-
-        <p>You are free to:</p>
-        <ul type="disc">
-          <li><b>Read and Print</b> our articles and other media free of charge.</li>
-          <li><b>Share and Reuse</b> our articles and other media under free and open licenses.</li>
-          <li><b>Contribute To and Edit</b> our pages and projects..</li>
-         </ul>
-        <h6>Under the following conditions:</h6>
-        <ul type="disc">
-          <li><b>Responsibility –</b> You take responsibility for your edits (since we only host your content).</li>
-          <li><b>Civility –</b> You support a civil environment and do not harass other users.</li>
-          <li><b>Lawful Behavior – </b>You do not violate copyright or other laws.</li>
-          <li><b>No Harm –</b> You do not harm our technology infrastructure.</li>
-          <li><b>Terms of Use and Policies –</b> You adhere to the Terms of Use and to the applicable community policies when you visit Canonizer or participate in our communities.</li>
-        </ul> 
-        
-        <h6>With the understanding that:</h6>
-        <ul type="disc">
-          <li><b>You License Freely Your Contributions –</b> you generally must license your contributions and edits to our sites or Projects under a free and open license (unless your contribution is in the public domain).</li>
-          <li><b>No Professional Advice –</b> the content of articles and other projects is for informational purposes only and does not constitute professional advice.</li>
-      </ul>
+    <p class="p1"><span class="s1">Terms of Service</span></p>
+    <p class="p2"><span class="s1"><br>
+    </span></p>
+    <p class="p3"><span class="s1">You are free to:</span></p>
+    <ul class="ul1">
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>Read and Print</b> our articles and other media free of charge.</span></li>
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>Share and Reuse</b> our articles and other media under free and open licenses.</span></li>
+      <li class="li3"><span class="s4"><b></b></span><span class="s1"><b>Contribute To and Edit</b> our pages and projects.</span></li>
+    </ul>
+    <p class="p3"><span class="s1">Under the following conditions:</span></p>
+    <ul class="ul1">
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>Responsibility –</b> You take responsibility for your edits (since we only host your content).</span></li>
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>Civility –</b> You support a civil environment and do not harass other users.</span></li>
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>Lawful Behavior – </b>You do not violate copyright or other laws.</span></li>
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>No Harm –</b> You do not harm our technology infrastructure.</span></li>
+      <li class="li3"><span class="s4"><b></b></span><span class="s1"><b>Terms of Use and Policies –</b> You adhere to the Terms of Use and to the applicable community policies when you visit Canonizer or participate in our communities.</span></li>
+    </ul>
+    <p class="p3"><span class="s1">With the understanding that:</span></p>
+    <ul class="ul1">
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>You License Freely Your Contributions –</b> you generally must license your contributions and edits to our sites or Projects under a free and open license (unless your contribution is in the pub:lic domain).</span></li>
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>No Professional Advice –</b> the content of articles and other projects is for informational purposes only and does not constitute professional advice.</span><span class="s1"><br>
+    </span></li>
+    </ul>
+    <p class="p3"><span class="s1"><b>User Identity</b></span></p>
+    <ul class="ul1">
+      <li class="li3"><span class="s4"><b></b></span><span class="s1"><b>Canonizer reserves the right to require users to verify their identities. </b>Users who prove to be deliberately disruptive may be flagged as “not verified” and may see their account access limited or denied altogether.<span class="Apple-converted-space"> </span></span></li>
+      <li class="li5"><b>Singular Identity</b> - Each Canonizer account must be associated with a single person, not a group of people or an organization.<span class="Apple-converted-space">  </span>No person can have more than one identity.</li>
+      <li class="li5"><b>Human Identity </b>- Bots are not permitted on Canonizer.<span class="Apple-converted-space"> </span></li>
+      <li class="li5"><b>Non-Transferable</b> - Users are not permitted to transfer their Canonizer account to someone else.<span class="Apple-converted-space"> </span></li>
+      <li class="li5"><b>Accurate Information</b> - Users who deliberately provide inaccurate information about their identity may be subject to account limitations, up to and including banning from the site.<span class="Apple-converted-space"> </span></li>
+    </ul>
+    <p class="p6"><span class="s1"></span><br></p>
+    <p class="p3"><span class="s1"><b>Disruptive Behavior</b></span></p>
+    <ul class="ul1">
+      <li class="li4"><span class="s2"><b></b></span><span class="s3"><b>Disruptive behavior can be defined as: </b>attempts to censor other users by any method.</span></li>
+      <li class="li4"><span class="s2"></span><span class="s3">Harassment</span></li>
+      <li class="li4"><span class="s2"></span><span class="s3">Libel</span></li>
+      <li class="li4"><span class="s2"></span><span class="s3">Hate speech</span></li>
+      <li class="li4"><span class="s2"></span><span class="s3">Use of sock puppets/multiple identities</span></li>
+      <li class="li4"><span class="s2"></span><span class="s3">Cloning the username of other users</span></li>
+      <li class="li4"><span class="s2"></span><span class="s3">Holding camps hostage by refusing to allow the camp to make the modifications it wants</span><span class="s1"><br>
+    </span></li>
+    </ul>
+    <p class="p3"><span class="s1"><b>Censorship</b></span></p>
+    <ul class="ul1">
+      <li class="li4"><span class="s2"></span><span class="s3">Canonizer is committed to avoiding censorship and allowing everyone to have a voice. That should not be interpreted as a license for behavior that shuts down legitimate voices through targeted disruption as outlined above. Those who work to limit the participation and expression of others leave us with no choice but to establish boundaries and limits on disruptors. This will make for an inclusive and productive conversation for all people of good will, which is what Canonizer is all about.</span></li>
+    </ul>
 
     </div>
     <!-- /.container-fluid-->
