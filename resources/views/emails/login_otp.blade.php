a8e1cd5cde4b082710f722b316171358114cf47f
Kshitij Singh <kshitij.singh@iffort.com>
2022-09-07 12:56:40 +0530

    Fixes #1559

--- resources/views/emails/login_otp.blade.php
+++ resources/views/emails/login_otp.blade.php
@@ -2,7 +2,7 @@
  Hello {{ $user->first_name}} {{ $user->last_name}},<br/>
  
  <p>Your one time verification code is: {{$user->otp}}</p>
-<p>If you ever have any issues or feedback, feel free to e-mail: {{ config('app.support_email') }}.</p>
+<p>If you ever have any issues or feedback, feel free to e-mail: <a href="mailto:{{ config('app.support_email') }}">{{ config('app.support_email') }}</a>.</p>
 
 Sincerely,<br>
 {{ config('app.email_signature') }}
--- resources/views/emails/registeration_otp.blade.php
+++ resources/views/emails/registeration_otp.blade.php
@@ -3,7 +3,7 @@
  <p>Thank you for registering an account with canonizer.com.</p>
 
  <p>Your one time verification code is: {{$user->otp}}</p>
-<p>If you ever have any issues or feedback, feel free to e-mail: {{ config('app.support_email') }}.</p>
+<p>If you ever have any issues or feedback, feel free to e-mail: <a href="mailto:{{ config('app.support_email') }}">{{ config('app.support_email') }}</a>.</p>
 
 Sincerely,<br>
 {{ config('app.email_signature') }}
